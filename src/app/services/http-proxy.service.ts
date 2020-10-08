e75c3d86410482aaebe216f52a42caf161babe67
parusov.u <parusov.u@medindex.ru>
2020-10-08 15:24:18 +0300

    add mock data

--- src/app/services/http-proxy.service.ts
+++ src/app/services/http-proxy.service.ts
@@ -10,7 +10,36 @@ export class HttpProxyService {
   public getQuestions(questionId: string): Observable<QuestionModel[]> {
     const result: QuestionModel[] = [];
     // return this.http.get<QuestionModel[]>(`your api/${questionId}`);
-
+    result.push(new QuestionModel(1, 'What is OOP?', null, null));
+    result.push(new QuestionModel(2, 'Are you working?', null, ['Yes', 'No']));
+    result.push(
+      new QuestionModel(3, 'Do you want to work?', null, [
+        'Yes',
+        'No',
+        "I don't know",
+      ])
+    );
+    result.push(
+      new QuestionModel(4, 'Which patterns do you know?', null, null)
+    );
+    result.push(
+      new QuestionModel(5, 'Do you want to work?', null, [
+        'Yes',
+        'No',
+        "I don't know",
+      ])
+    );
+    result.push(
+      new QuestionModel(6, 'What year have you been working?', null, null)
+    );
+    result.push(
+      new QuestionModel(
+        7,
+        'What grade do you give for this questionnaire?',
+        null,
+        ['1', '2', '3', '4', '5', '6', '7', '8', '9', '10']
+      )
+    );
     return of(result);
   }
 }
